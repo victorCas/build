32b06aa811c5c84150496577d8ecf9448da28ad8
Victor Jansson <victor.jansson@casumo.com>
2016-09-20 14:51:48 +0200

    Added babel transpiling and automatic index.html generation

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,32 @@
+const webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
     entry: './web/src/js/app.js',
+
     output: {
         path: './web/build',
         filename: 'app.bundle.js'
-    }
+    },
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            query: {
+                presets: ['es2015']
+            }
+        }]
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            },
+            output: {
+                comments: false
+            }
+        }),
+        new HtmlWebpackPlugin()
+    ]
 };
